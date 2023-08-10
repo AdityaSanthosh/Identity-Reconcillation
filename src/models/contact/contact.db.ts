3bc973aa70518dd69d0a18abea7dfab027e96a4c
santhoshaditya2000 <aditya@velocity.in>
2023-08-10 20:33:28 +0530

    logic improvements

--- src/models/contact/contact.db.ts
+++ src/models/contact/contact.db.ts
@@ -1,44 +1,53 @@
 import { Contact } from '@server/models/contact/contact.interface';
 import { parseContact } from '@server/models/contact/contact.utils';
+import {LinkPrecendence, Prisma} from "@prisma/client";
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+// @ts-ignore
 export async function createContact(data): Promise<Contact> {
-    const contact = await prisma.contact.create({
-      data: {
-        phoneNumber: data.phoneNumber || null,
-        email: data.email || null,
-        linkedId: data.linkedId || null,
-        linkPrecedence: data.linkPrecedence || null,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    });
-    return parseContact(contact);
+    try {
+        const contact = await prisma.contact.create({
+            data: {
+                phoneNumber: data.phoneNumber || null,
+                email: data.email || null,
+                linkedId: data.linkedId || null,
+                linkPrecedence: data.linkPrecedence || null,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        });
+        return parseContact(contact);
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            // The .code property can be accessed in a type-safe manner
+            if (e.code === 'P2002') {
+                return parseContact(await getContactbyEmailandPhoneNumber(data.email, data.phoneNumber));
+            }
+        }
+    }
 }
 
 export async function getContactById(id): Promise<Contact> {
-    return prisma.contact.findUnique({
+    return parseContact(await prisma.contact.findUnique({
         where: {id},
-    });
+    }));
 }
 
 export async function updateContact(id, data) {
-    return prisma.contact.update({
+    return await prisma.contact.update({
         where: {id},
         data: {
-            phoneNumber: data?.phoneNumber,
-            email: data?.email,
-            linkedId: data?.linkedId,
-            linkPrecedence: data?.linkPrecedence,
+            linkedId: data.linkedId,
+            linkPrecedence: data.linkPrecedence,
             updatedAt: new Date(),
         },
     });
 }
 
 export async function getSecondaryContacts(id): Promise<Contact[]> {
-    return await  prisma.contact.findMany({
+    let contacts = await prisma.contact.findMany({
         where: {
             linkedId: id
         },
@@ -46,4 +55,28 @@ export async function getSecondaryContacts(id): Promise<Contact[]> {
             createdAt: 'asc'
         }
     });
+    return contacts.map(contact=>parseContact(contact));
+}
+
+export async function getContactbyEmailandPhoneNumber(email: string, phoneNumber: string): Promise<Contact> {
+    return prisma.contact.findFirst({
+        where: {
+            email,
+            phoneNumber,
+        },
+    });
+}
+
+export async function makeSecondary(linkedId, targetLinkedId) {
+    if(linkedId!==targetLinkedId) {
+        await prisma.contact.updateMany({
+            where: {
+                linkedId
+            },
+            data: {
+                linkedId: targetLinkedId,
+                linkPrecedence: LinkPrecendence.secondary
+            }
+        })
+    }
 }
--- src/models/contact/contact.path.ts
+++ src/models/contact/contact.path.ts
@@ -3,16 +3,35 @@ import {
   ContactResponse,
 } from '@server/models/contact/contact.interface';
 import {
-    createContact,
-    getContactById, getSecondaryContacts,
-    updateContact,
+  createContact,
+  getContactbyEmailandPhoneNumber,
+  getContactById,
+  getSecondaryContacts,
+  makeSecondary,
+  updateContact,
 } from '@server/models/contact/contact.db';
-import {LinkPrecendence, PrismaClient} from '@prisma/client';
-import {convertContact} from "@server/models/contact/contact.utils";
+import { LinkPrecendence, PrismaClient } from '@prisma/client';
+import {
+  convertContact,
+  parseContact,
+} from '@server/models/contact/contact.utils';
 
 const prisma = new PrismaClient();
 
+function getOldestAndRecent(obj1, obj2) {
+    if (!obj1) return { oldest: obj2, recent: null };
+    if (!obj2) return { oldest: obj1, recent: null };
+    let oldest = obj1.createdAt < obj2.createdAt ? obj1 : obj2;
+    let recent = obj1 == oldest ? obj2: obj1;
+    return {oldest, recent };
+}
+
+
 export async function create(email, phoneNumber): Promise<Contact> {
+    let existingContact = await getContactbyEmailandPhoneNumber(email,phoneNumber)
+    if(existingContact) {
+        return existingContact;
+    }
     const existingEmailContact = email
         ? await prisma.contact.findFirst({
             where: { email },
@@ -25,32 +44,39 @@ export async function create(email, phoneNumber): Promise<Contact> {
             orderBy: { createdAt: 'asc' },
         })
         : null;
-    // Don't create a new contact. Since there is a Unique Phone & Email db check, alternate implementation is throwing this from
-    // createContact function
-    if(existingPhoneContact && existingEmailContact && existingEmailContact.id===existingPhoneContact.id) {
-        return existingPhoneContact;
-    }
-    if(existingPhoneContact && existingEmailContact && existingEmailContact.id!==existingPhoneContact.id) {
-        const [oldest_contact, recent_contact] =
-            existingPhoneContact.createdAt < existingEmailContact.createdAt
-                ? [existingPhoneContact, existingEmailContact]
-                : [existingEmailContact, existingPhoneContact];
-        await updateContact(recent_contact.id,{linkPrecedence:LinkPrecendence.secondary, linkedId: oldest_contact.id});
-    }
     let linkPrecedence;
     let linkedId;
+
     // No Existing Contact found, create a new contact
     if(!existingPhoneContact && !existingEmailContact) {
         linkPrecedence = LinkPrecendence.primary;
     }
-    // If either phoneNumber or Email is found, get the primary contact
-    // and make its id as the linkedId of the newly created contact with linkPrecedence Secondary
+
     if(existingPhoneContact || existingEmailContact) {
-        let firstIdentifiedId = existingPhoneContact?.id || existingEmailContact?.id;
-        let firstIdentifiedContact = await getContactById(firstIdentifiedId);
-        let primaryContact = firstIdentifiedContact.linkPrecedence === LinkPrecendence.secondary ? await getContactById(firstIdentifiedContact.linkedId) : firstIdentifiedContact;
-        linkedId = primaryContact.id;
-        linkPrecedence = LinkPrecendence.secondary
+        let contacts =
+            getOldestAndRecent(existingPhoneContact, existingEmailContact);
+        let oldest_contact = contacts.oldest;
+        let recent_contact = contacts.recent;
+        if(oldest_contact.linkedId) {
+            oldest_contact = await getContactById(oldest_contact.linkedId);
+        }
+        if(recent_contact) {
+            if(recent_contact.linkedId) {
+                let contact = await getContactById(recent_contact.linkedId);
+                await updateContact(contact.id, {linkPrecedence, linkedId: oldest_contact.id});
+                await makeSecondary(contact.id, oldest_contact.id);
+                if(contact.createdAt < oldest_contact.createdAt) {
+                    oldest_contact = contact;
+                    recent_contact = contacts.oldest;
+                }
+            }
+        }
+        linkedId = oldest_contact.id;
+        linkPrecedence = LinkPrecendence.secondary;
+        if(recent_contact) {
+            await updateContact(parseContact(recent_contact).id, {linkPrecedence,linkedId});
+            await makeSecondary(recent_contact.id,linkedId);
+        }
     }
     let new_contact = {
         phoneNumber,
@@ -68,10 +94,14 @@ export async function identify(contact: Contact): Promise<ContactResponse> {
         const allContacts = await getSecondaryContacts(contact.id);
         const secondaryContactIds = allContacts.map(contact => contact.id);
         allContacts.unshift(contact);
+        const raw_emails = allContacts.map(contact => contact.email);
+        const emails = [...new Set(raw_emails)];
+        const raw_phoneNumbers = allContacts.map(contact => contact.phoneNumber);
+        const phoneNumbers = [...new Set(raw_phoneNumbers)];
         data = {
             primaryContactId: contact.id,
-            emails: allContacts.map(contact => contact.email),
-            phoneNumbers: allContacts.map(contact => contact.phoneNumber),
+            emails,
+            phoneNumbers,
             secondaryContactIds,
         }
     }
